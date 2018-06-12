49eb0b5bd479733b2cb3c1ff10d7a2cf46469820
Andrey Azov <andrey.azov@gmail.com>
2018-06-13 02:00:10 +0300

    Add prod scripts; add router to serve index.html

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const uuidMiddleware = require('./middleware/uuid-middleware');
 const uploadRouter = require('./routers/upload-router');
 
 const app = express();
+const pathToStatic = path.resolve(__dirname, '../build');
+const pathToHTML = path.resolve(pathToStatic, 'index.html');
 
 // add an id to every request
 app.use(uuidMiddleware);
 
+app.use(express.static(pathToStatic));
+
 app.use('/upload', uploadRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(pathToHTML);
+});
 
 module.exports = app;
